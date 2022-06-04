6791c76e693aeeb168f7661983ce2f7d0c2996bb
kdbvier <kdbvier@gmail.com>
2022-06-04 19:08:17 +0800

    new: first commit

---
+++ src/components/App/App.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import BpkText from 'bpk-component-text';
+
+import Header from '../Header';
+
+import STYLES from './App.scss';
+
+const getClassName = className => STYLES[className] || 'UNKNOWN';
+
+const App = () => (
+  <div className={getClassName('App')}>
+    <Header />
+    <main className={getClassName('App__main')}>
+      <BpkText tagName="p">Over to you...</BpkText>
+      {/* TODO: Add a component to display results here */}
+    </main>
+  </div>
+);
+
+export default App;
